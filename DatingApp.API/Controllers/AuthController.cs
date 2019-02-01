0c90d5dbc504245643f3f2ee47a123a828f9ad01
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-01 22:48:12 +0100

    Added User registration validation

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -20,8 +20,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // validate request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await repo.UserExists(userForRegisterDto.Username)) return BadRequest("Username already exists");
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        public string Username { get; set; }
+        [Required] public string Username { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "You must specify password between 8 and 20 characters")]
         public string Password { get; set; }
     }
 }
