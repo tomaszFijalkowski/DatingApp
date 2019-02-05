0fe280369a5366157c8e0f96af82e553092d8a65
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-05 23:24:47 +0100

    Added UserSeedData to populate Database

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext context;
+
+        public Seed(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public void SeedUsers()
+        {
+            var userData = File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+            foreach (var user in users)
+            {
+                CreatePasswordHash("password", out var passwordHash, out var passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Username = user.Username.ToLower();
+
+                context.Users.Add(user);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
