427e564def5b53ca09c792199dff0c60fb4cd7fa
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-13 22:52:50 +0100

    Add UserForUpdateDto

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Helpers
                     opt => { opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url); })
                 .ForMember(dest => dest.Age, opt => { opt.ResolveUsing(d => d.DateOfBirth.CalculateAge()); });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
