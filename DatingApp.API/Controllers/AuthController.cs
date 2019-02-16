dd314f0727530173af3070d45bcec93a4c83e907
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-16 16:12:26 +0100

    Make UsersController log activity

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -36,14 +36,13 @@ namespace DatingApp.API.Controllers
 
             if (await repo.UserExists(userForRegisterDto.Username)) return BadRequest("Username already exists");
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await repo.Register(userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
+            var userToReturn = mapper.Map<UserForDetailedDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
