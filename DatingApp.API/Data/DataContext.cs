94bbce1799d5e0b8d9061c6bdba1276dcf299e5f
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-16 21:19:23 +0100

    Create many-to-many relation for Likes

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -12,5 +12,24 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(k => new {k.LikerId, k.LikedId});
+            
+            builder.Entity<Like>()
+                .HasOne(u => u.Liked)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikedId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Liked)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
