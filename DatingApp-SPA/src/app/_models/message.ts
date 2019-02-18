fc51b352c634b03eaba556a9bdce97266ce2e09e
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-18 19:31:10 +0100

    Add message interface

---
+++ DatingApp-SPA/src/app/_models/message.ts
@@ -0,0 +1,13 @@
+export interface Message {
+    id: number;
+    senderId;
+    senderKnownAs: string;
+    senderPhotoUrl: string;
+    recipientId: number;
+    recipientKnownAs: string;
+    recipientPhotoUrl: string;
+    content: string;
+    isRead: boolean;
+    dateRead: Date;
+    messageSent: Date;
+}
