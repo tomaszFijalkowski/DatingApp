b3a8e637a4109b6a979780767c7dbcfb40755a1f
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-18 19:34:41 +0100

    Add MessageParams

---
+++ DatingApp.API/Helpers/MessageParams.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DatingApp.API.Helpers
+{
+    public class MessageParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = Math.Min(MaxPageSize, value);
+        }
+
+        public int UserId { get; set; }
+        public string MessageContainer { get; set; } = "Unread";
+    }
+}
