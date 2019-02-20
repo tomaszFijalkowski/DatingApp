bb9a80da9c56e8fcba3e59046501b8b0d1a97f84
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-20 06:19:43 +0100

    Update Seed for Identity

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -1,46 +1,29 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
 using Newtonsoft.Json;
 
 namespace DatingApp.API.Data
 {
     public class Seed
     {
-        private readonly DataContext context;
+        private readonly UserManager<User> userManager;
 
-        public Seed(DataContext context)
+        public Seed(UserManager<User> userManager)
         {
-            this.context = context;
+            this.userManager = userManager;
         }
 
         public void SeedUsers()
         {
+            if (userManager.Users.Any()) return;
+
             var userData = File.ReadAllText("Data/UserSeedData.json");
             var users = JsonConvert.DeserializeObject<List<User>>(userData);
-            foreach (var user in users)
-            {
-                CreatePasswordHash("password", out var passwordHash, out var passwordSalt);
 
-                user.PasswordHash = passwordHash;
-                user.PasswordSalt = passwordSalt;
-                user.Username = user.Username.ToLower();
-
-                context.Users.Add(user);
-            }
-
-            context.SaveChanges();
-        }
-
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            using (var hmac = new HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-            }
+            foreach (var user in users) userManager.CreateAsync(user, "password").Wait();
         }
     }
 }
